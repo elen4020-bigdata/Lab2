40f2dd2a60288d5369a360c76c3eb96e42168d4f
Nicholas Kastanos <nicholaskastanos@gmail.com>
2019-03-01 14:50:39 +0200

    Diagonalizing the threads

--- matrixTransposePthreads.cpp
+++ matrixTransposePthreads.cpp
@@ -8,27 +8,46 @@
 
 using namespace std;
 
-#define NUM_THREADS 4
-
 struct thread_data{
+    int i;
 	shared_ptr<vector<shared_ptr<vector<int32_t>>>> A;
 };
 
-void *transpose(void *threadarg){
-	struct thread_data *data;
-	data = (struct thread_data *) threadarg;
-	//auto A = data -> A;
-	for(auto i = 0; i < data ->A -> size(); i++){
-		for(auto j = (i + 1); j < data -> A -> size(); j++){
-			data -> A -> at(i) -> at(j) = data ->A -> at(i) -> at(j) + data ->A -> at(j) -> at(i);
-			data ->A -> at(j) -> at(i) = data ->A -> at(i) -> at(j) - data ->A -> at(j) -> at(i);
-			data ->A -> at(i) -> at(j) = data ->A -> at(i) -> at(j) - data ->A -> at(j) -> at(i);
-		}
+void *diagonal(void *threadarg){
+    struct thread_data *data;
+    data = (struct thread_data *) threadarg;
+    cout << "i in function = " << data -> i << endl;
+    auto k = data -> i;
+    for(auto j = (k + 1); j < data -> A -> size(); j++){
+		data -> A -> at(k) -> at(j) = data ->A -> at(k) -> at(j) + data ->A -> at(j) -> at(k);
+		data ->A -> at(j) -> at(k) = data ->A -> at(k) -> at(j) - data ->A -> at(j) -> at(k);
+		data ->A -> at(k) -> at(j) = data ->A -> at(k) -> at(j) - data ->A -> at(j) -> at(k);
 	}
-    pthread_exit(NULL);
+    cout << "i after it is used = " << data -> i << endl;
 }
 
-
+void transpose(shared_ptr<vector<shared_ptr<vector<int32_t>>>> A){
+    int sizeMatrix = A -> size();
+    pthread_t threads[sizeMatrix];
+    int rc;
+    struct thread_data data;
+    void *status;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    data.A = A;
+    data.i = 0;
+	for(auto i = 0; i < A -> size(); i++){
+        cout << "i in loop = " << i << endl;
+		rc = pthread_create(&threads[i], NULL, diagonal, (void*)&data);
+        data.i = data.i + 1;
+	}
+    pthread_attr_destroy(&attr);
+    for(auto i = 0; i < A -> size(); i++){
+	    rc = pthread_join(threads[i], &status);
+    }
+    cout << "completed transpose" << endl;
+}
 
 class DimensionsHaveToBeNonZeroPositiveIntegersException{};
 
@@ -66,16 +85,16 @@ int main(){
 
     auto A = Generate2DArray(4);
 
-    pthread_t threads[NUM_THREADS];
+    /*pthread_t threads[NUM_THREADS];
     int rc;
     struct thread_data data;
     void *status;
     pthread_attr_t attr;
-    data.A = A;
+    data.A = A;*/
 
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-
+    /*pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);*/
+/*
     for(auto i = 0; i < NUM_THREADS; i++){
 	    rc = pthread_create(&threads[i], NULL, transpose, (void*)&data);
     }
@@ -83,15 +102,16 @@ int main(){
     pthread_attr_destroy(&attr);
     for(auto i = 0; i < NUM_THREADS; i++){
 	    rc = pthread_join(threads[i], &status);
-    }
+    }*/
+    transpose(A);
+    cout << "back in main" << endl;
     cout << endl;
     for (auto i = 0; i < 4 ; i++){
         for(auto j = 0; j < 4 ; j++){
-            cout << data.A->at(i)->at(j) << " ";
+            cout << A->at(i)->at(j) << " ";
         }
         cout <<endl;
     }
     pthread_exit(NULL);
-    
     return 0;
 }
