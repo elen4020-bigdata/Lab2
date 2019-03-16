2416f0b1ae770b30ed564586e48f2556b62afa7a
Anita <demellokochanita@gmail.com>
2019-03-16 10:39:30 +0200

    Pthreads works for everything except super huge

--- testPthreads.cpp
+++ testPthreads.cpp
@@ -6,6 +6,7 @@
 #include <memory>
 
 using namespace std;
+using namespace std::chrono;
 
 class DimensionsHaveToBeNonZeroPositiveIntegersException{};
 
@@ -57,22 +58,22 @@ struct newData{
     bool swap;
 };
 
-template<class T, int n>
-void *swap(void *args){
-    struct newData<T,n> *data;
-    data = (struct newData<T,n> *) args;
-    auto A = data -> A;
-    auto i = data -> i;
-    auto l = data -> l;
-    auto j = data -> j;
-    auto k = data -> k;
-    auto swap = data -> swap;
-    if((i == l && swap) || (i != l)){
-        auto temp = A->at(i+j).at(l+k);
-        A->at(i+j).at(l+k) = A->at(l+k).at(i+j);
-        A->at(l+k).at(i+j) = temp;
-    }
-}
+// template<class T, int n>
+// void *swap(void *args){
+//     struct newData<T,n> *data;
+//     data = (struct newData<T,n> *) args;
+//     auto A = data -> A;
+//     auto i = data -> i;
+//     auto l = data -> l;
+//     auto j = data -> j;
+//     auto k = data -> k;
+//     auto swap = data -> swap;
+//     if((i == l && swap) || (i != l)){
+//         auto temp = A->at(i+j).at(l+k);
+//         A->at(i+j).at(l+k) = A->at(l+k).at(i+j);
+//         A->at(l+k).at(i+j) = temp;
+//     }
+// }
 
 template<class T, int n>
 void *Transpose(void *args){
@@ -82,38 +83,25 @@ void *Transpose(void *args){
     auto l = data -> l;
     auto A = data -> A;
     auto blockSize = data -> blockSize;
-    pthread_t threads[blockSize*blockSize];
-    struct newData<T,n> newData[blockSize*blockSize];
-    void *status;
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    for(auto q = 0; q < (blockSize*blockSize); q++){
-		newData[q].A = A;
-		newData[q].i = i;
-        newData[q].l = l;
-	}
-    auto iter = 0;
     for(auto j = 0; j < (blockSize); j++){
         for(auto k = 0; k < (blockSize); k++){
-            newData[iter].j = j;
-            newData[iter].k = k;
-            newData[iter].swap = true;
-            if(i == l && (iter%2 == 0)){
-                newData[iter].swap = false;
+            for(auto j = 0; j < (blockSize); j++){
+                for(auto k = 0; k < (blockSize); k++){
+                    auto temp = A->at(i+j).at(l+k);
+                    A->at(i+j).at(l+k) = A->at(l+k).at(i+j);
+                    A->at(l+k).at(i+j) = temp;
+                    if(i == l){
+                        break;
+                    }
+                }
             }
-            pthread_create(&threads[iter],NULL, swap<T,n>, (void*)&newData[iter]);
-            iter++;
         }
-    }
-    pthread_attr_destroy(&attr);
-    for(auto i = 0; i < blockSize; i++){
-	    pthread_join(threads[i], &status);
     }
 }
 
 template<class T, int n, int blockSize>
 void blockTranspose(array<array<T, n>,n>* A){
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
     int numThreads = ((n/blockSize)*(n/blockSize)/2)+((n/blockSize)/2);
     pthread_t threads[numThreads];
     struct thread_data<T, n> data[numThreads];
@@ -133,20 +121,22 @@ void blockTranspose(array<array<T, n>,n>* A){
             pthread_create(&threads[iter], NULL, Transpose<T,n>, (void*)&data[iter]);
             iter++;
         }
-        printf("still going");
     }
     pthread_attr_destroy(&attr);
     for(auto i = 0; i < numThreads; i++){
     	pthread_join(threads[i], &status);
     }
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+    std::cout << "The operation took: " << time_span.count() << " seconds.";
 }
 
 
 int main(){
-    auto A = new array<array<int32_t, 1024>,1024>;
-    Generate2DArray<int32_t,1024>(A);
+    auto A = new array<array<int32_t, 16384>,16384>;
+    Generate2DArray<int32_t,16384>(A);
     cout << "transposing" << endl;
-    blockTranspose<int32_t, 1024, 16>(A);
+    blockTranspose<int32_t, 16384, 32>(A);
     cout << endl;
     // for (auto i = 0; i < 4 ; i++){
     //     for(auto j = 0; j < 4 ; j++){
